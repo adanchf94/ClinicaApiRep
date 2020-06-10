b7538704603618c32f32c658e9c9ad04ab7fed2c
adanchf94 <adanchf94@hotmail.com>
2020-06-10 14:18:19 -0600

    Added especialidad controller

---
+++ ClinicaApi/ClinicaApi/Controllers/EspecialidadController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ConectarDatos;
+using System.Data.Entity;
+
+namespace ClinicaApi.Controllers
+{
+    public class EspecialidadController : ApiController
+    {
+
+        private ClinicaDBEntitiesConn DbContext = new ClinicaDBEntitiesConn();
+
+        [HttpGet]
+        public IEnumerable<Especialidad> Get()
+        {
+            using (ClinicaDBEntitiesConn ClinicaEntities = new ClinicaDBEntitiesConn())
+            {
+                return ClinicaEntities.Especialidads.ToList();
+            }
+        }
+
+        [HttpGet]
+        public Especialidad Get(string id)
+        {
+            using (ClinicaDBEntitiesConn ClinicaEntities = new ClinicaDBEntitiesConn())
+            {
+                return ClinicaEntities.Especialidads.FirstOrDefault(e => e.IdEspecialidad == id);
+            }
+        }
+
+    }
+}
